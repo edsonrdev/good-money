0e9ccf90c7ce1896205045d6df6337705be46c5e
Edson Rodrigues <edsonr.dev@gmail.com>
2022-06-29 10:51:05 -0300

    refactor: move new transaction modal to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,42 @@
+import { useState } from "react";
+
 import { GlobalStyle } from "./styles/global";
+
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
 import { TransactionsTable } from "./components/TransactionsTable";
 
+import Modal from "react-modal";
+Modal.setAppElement("#root");
+
 export const App = () => {
+  /*
+    -------------------------------------------------------------
+    PROPS NECESSÁRIAS PRO MODAL
+    -------------------------------------------------------------
+    isOpen -> aberto / fechado
+    onRequestClose -> o que acontecer nos eventos de fechar modal
+    -------------------------------------------------------------
+  */
+
+  const [newTransactionModalOpen, setNewTransactionModalOpen] = useState(false);
+
+  // callbacks para abrir / fechar modal de transações
+  const openNewTransactionModal = () => setNewTransactionModalOpen(true);
+  const closeNewTransactionModal = () => setNewTransactionModalOpen(false);
   return (
     <>
       <GlobalStyle />
-      <Header />
+      <Header onOpenNewTransactionModal={openNewTransactionModal} />
       <Dashboard />
       <TransactionsTable />
+
+      <Modal
+        isOpen={newTransactionModalOpen}
+        onRequestClose={closeNewTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
     </>
   );
 };
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,18 @@
-import logoImg from "../../assets/logo.svg";
 import { Container, Content } from "./styles";
+import logoImg from "../../assets/logo.svg";
 
-export const Header = () => {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export const Header = ({ onOpenNewTransactionModal }: HeaderProps) => {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="Good Money" />
-        <button type="button">Nova transação</button>
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova transação
+        </button>
       </Content>
     </Container>
   );
