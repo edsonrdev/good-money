b661fae200a3c42c7c18784f7094d8d0b5e13b0e
Edson Rodrigues <edsonr.dev@gmail.com>
2022-06-29 12:19:48 -0300

    feat/style: create and style TransactionTypeContainer (contains transaction type buttons)

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from "react-modal";
-import { Container } from "./styles";
+import { Container, TransactionTypeContainer } from "./styles";
 import closeImg from "../../assets/close.svg";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -30,7 +32,19 @@ export const NewTransactionModal = ({
         <h2>Cadastrar transação</h2>
 
         <input type="text" placeholder="Título" />
+
         <input type="number" min="0" step="0.01" placeholder="Valor" />
+
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Entradas" /> Entradas
+          </button>
+
+          <button type="button">
+            <img src={outcomeImg} alt="Saídas" /> Saídas
+          </button>
+        </TransactionTypeContainer>
+
         <input type="text" placeholder="Categoria" />
 
         <button type="submit">Cadastrar</button>
